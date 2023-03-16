dfcc3058f8c18681483bdadcf57957d602a61ec8
Ophelia <ophelia.lavey2@gmail.com>
2023-03-17 00:51:12 +0100

    Added comments to Track.cpp and Track.h

--- Source/Track.cpp
+++ Source/Track.cpp
@@ -4,11 +4,15 @@
     Track.cpp
     Created: 26 Feb 2023 10:50:24am
     Author:  Ophelia
+    Purpose: stores the data for a single audio track
 
   ==============================================================================
 */
 
 #include "Track.h"
+
+
+/** Constructor: defines private data members */
 Track::Track(
     unsigned __int64 _rowNumber,
     juce::URL _url,
@@ -31,71 +35,76 @@ Track::~Track() {
 
 }
 
-// Getters for the data members
+//========================================Getters for the Private Data Members==============================================
+
+/** Returns track's index/row number */
 unsigned __int64 Track::getRowNumber()
 {
     return rowNumber;
 }
+/** Returns track's URL */
 juce::URL Track::getUrl()
 {
     return url;
 }
+/** Returns track's title */
 std::string Track::getTitle()
 {
     return title;
 }
+/** Returns track's file extension, e.g. "mp3" */
 std::string Track::getExtensionName()
 {
     return extensionName;
 }
+/** Returns track duration as a HH:MM:SS string */
 std::string Track::getDuration()
 {
     return duration;
 }
+/** Returns track's absolute file path */
 std::string Track::getFilePath()
 {
     return filePath;
 }
-
+/** Returns Boolean whihc stores whether the track should be displayed based on the user's searchbox input string */
 bool Track::getIsDisplayed()
 {
     return isDisplayed;
 }
 
-/** This function decides whether the track info should be displayed(after user searches for tracks)
-    and sets the "isDisplayed" function to false if the track does not meet the search input criteria and
-    true if it does. Takes in the search input as a parameter to check if the track meets the search criteria
-    by seeing if the track data (title, extension) includes this substring. Calls the isDisplayed setter.
+//========================================================================================================================
+
+/**
+ *Determines whether the track info should be displayed (after user searches for specific terms in the search box)
+ *and sets the "isDisplayed" function to false if the track does not meet the search input criteria and
+ *true if it does. This takes in the search input as a parameter to check if the track meets the search criteria
+ *by seeing if the track data includes this substring, and then it calls the isDisplayed setter function.
 */
 void Track::determineIfShouldDisplay(const juce::String& searchInput)
 {   
-    // Make determination of whether track-info contains the entered string case-insensitive
+    // Makes determination of whether track-info contains the entered string case-insensitive
     juce::String lowerCaseSearchInput = searchInput.toLowerCase();
-    // Change the input string to a stringRef JUCE data type because the .contains() method takes a StringRef argument
+
+    // Changes the input string to a stringRef JUCE data type because the .contains() method takes a StringRef argument
     juce::StringRef stringRefSearchInput = juce::StringRef(lowerCaseSearchInput);
 
-    // Logic to check whether the track's title/extension match the user's search input
+    // Logic to check whether the track's title/duration match the user's search input
     if (juce::String(title).toLowerCase().contains(stringRefSearchInput)
         || juce::String(duration).toLowerCase().contains(stringRefSearchInput)
     )
     {   
-
         // If the track data contains the entered substring, set isDisplayed bool to "true"
-        DBG("Track::determineIfShouldDisplay - true");
         setIsDisplayed(true);
     }
     // Search box is empty --> display all the tracks
     else if (searchInput.isEmpty())
     {   
-        // If the track data contains the entered substring, set isDisplayed bool to "true"
-        DBG("Track::determineIfShouldDisplay - true");
         setIsDisplayed(true);
     }
     else
     {   
-
         // If the track data does not contain the entered substring, set isDisplayed bool to "false"
-        DBG("Track::determineIfShouldDisplay - false");
         setIsDisplayed(false);
     }
 }
--- Source/Track.h
+++ Source/Track.h
@@ -4,6 +4,7 @@
     Track.h
     Created: 26 Feb 2023 10:50:24am
     Author:  Ophelia
+    Purpose: stores the data for a single audio track
 
   ==============================================================================
   A track object which will store all the data for each added song to the playlistComponent
@@ -14,6 +15,7 @@
 
 class Track {
     public:
+        /** Constructor: defines private data members */
         Track(
             unsigned __int64  _rowNumber,
             juce::URL _url,
@@ -21,24 +23,35 @@ class Track {
             std::string _extensionName,
             std::string _duration,
             std::string _filePath,
-            // Default: all tracks are displayed before user searches for something
+            // Default: all tracks are displayed before user searches for a specific term
             bool _isDisplayed = true
         );
         ~Track();
 
-        // Getters for the data members
+        //========================================Getters for the Private Data Members==============================================
+
+        /** Returns track's index/row number */
         unsigned __int64 getRowNumber();
+        /** Returns track's URL */
         juce::URL getUrl();
+        /** Returns track's title */
         std::string getTitle();
+        /** Returns track's file extension, e.g. "mp3" */
         std::string getExtensionName();
+        /** Returns track duration as a HH:MM:SS string */
         std::string getDuration();
+        /** Returns track's absolute file path */
         std::string getFilePath();
+        /** Returns Boolean whihc stores whether the track should be displayed based on the user's searchbox input string */
         bool getIsDisplayed();
 
-        /** This function decides whether the track info should be displayed(after user searches for tracks)
-            and sets the "isDisplayed" function to false if the track does not meet the search input criteria and
-            true if it does. Takes in the search input as a parameter to check if the track meets the search criteria
-            by seeing if the track data includes this substring. Calls the isDisplayed setter.
+        //========================================================================================================================
+
+        /** 
+         *Determines whether the track info should be displayed (after user searches for specific terms in the search box)
+         *and sets the "isDisplayed" function to false if the track does not meet the search input criteria and
+         *true if it does. This takes in the search input as a parameter to check if the track meets the search criteria
+         *by seeing if the track data includes this substring, and then it calls the isDisplayed setter function.
         */
         void determineIfShouldDisplay(const juce::String& searchInput);
 
@@ -47,21 +60,22 @@ class Track {
 
     private:
 
-        /** Reason why this is an 'unsigned __int64' type :
-         * when creating the track when user selects + uploads a new file,
+        /** 
+         *Reason why this is an 'unsigned __int64' type :
+         * When creating the track when user selects + uploads a new file,
          * the tracks.size() property is used, which returns an unsigned __int64
          * If this is just an 'int', then the program does not compile, as an
-         * error is thrown due to a "narrowing conversion" from a huge to ordinary-sized integer
+         * error is thrown due to a "narrowing conversion" from a huge to ordinary-sized integer.
         */
         unsigned __int64  rowNumber;
 
+        /** Private track data members storing info about this audio track */
         juce::URL url;
         std::string title;
         std::string extensionName;
         std::string duration;
         std::string filePath;
 
-        // This stores whether the track should be displayed or not depending on whether user has searched for it
-        // The default is "true" --> before the user searches, all the tracks should be displayed
+        // This stores whether the track should be displayed or not depending on whether user has searched for it (default --> true)
         bool isDisplayed;
 };
